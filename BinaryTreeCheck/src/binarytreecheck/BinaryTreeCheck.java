a556098b38694f8535f99e5a29035c1c3efe0b97
Sravanthi Talluri <>
2017-11-15 19:14:24 +0530

    Binary Tree Check

--- BinaryTreeCheck/src/binarytreecheck/BinaryTreeCheck.java
+++ BinaryTreeCheck/src/binarytreecheck/BinaryTreeCheck.java
@@ -1,22 +1,65 @@
 
 package binarytreecheck;
 
-class Node {
-        int data;
-        Node left;
-        Node right;
-     }
+class Node
+{
+    int data;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
 
 public class BinaryTreeCheck {
 
-    boolean checkBST(Node root) {
-        return checkBT(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    // Root of the Binary Tree
+    Node root;
+ 
+    // To keep tract of previous node in Inorder Traversal
+    Node prev;
+ 
+    boolean isBST()  {
+        prev = null;
+        return isBST(root);
     }
-    boolean checkBT(Node root, int min, int max) {
-        if (root == null)
-            return true;
-        return (min < root.data && root.data < max) && 
-            checkBT(root.left, min, root.data) && checkBT(root.right, root.data, max);
+ 
+    /* Returns true if given search tree is binary
+       search tree (efficient version) */
+    boolean isBST(Node node)
+    {
+        // traverse the tree in inorder fashion and
+        // keep a track of previous node
+        if (node != null)
+        {
+            if (!isBST(node.left))
+                return false;
+ 
+            // allows only distinct values node
+            if (prev != null && node.data <= prev.data )
+                return false;
+            prev = node;
+            return isBST(node.right);
+        }
+        return true;
+    }
+ 
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+        BinaryTreeCheck tree = new BinaryTreeCheck();
+        tree.root = new Node(4);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(5);
+        tree.root.left.left = new Node(1);
+        tree.root.left.right = new Node(3);
+ 
+        if (tree.isBST())
+            System.out.println("IS BST");
+        else
+            System.out.println("Not a BST");
     }
     
 }
